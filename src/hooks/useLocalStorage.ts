9a04c2b884396f474aff68c6ea38c2b188732d5a
onitsiky <hei.onitsiky@gmail.com>
2023-06-15 11:44:14 +0300

    feat: create localStorage hook

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,5 +1,8 @@
-const useLocalStorage = () => {
+import {Task} from "@/pages/tasks";
 
+const useLocalStorage = (value: Task[]) => {
+  localStorage.setItem('Tasks', JSON.stringify(value))
+  return localStorage.getItem("Tasks")
 }
 
 export {
